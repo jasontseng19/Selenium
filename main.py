0e752294c363ade6dad7ddd7cb17ae958659ccca
jasontseng19 <41013759+jasontseng19@users.noreply.github.com>
2025-06-21 16:27:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--window-size=1920,1080')
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 options.add_experimental_option("prefs", {
     "profile.password_manager_enabled": False,
